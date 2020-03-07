433267c79faa18b7600e756eaeee7a7fa0a359f9
cris <crisliu@aliyun.com>
2020-03-07 21:40:35 +0800

    react router and inintial css files

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import LayoutCss from '@/assets/style/layout.module.scss';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        <ul>
+      <div className={LayoutCss.foot_wrapper}>
+        <ul className={LayoutCss.flex_content}>
           <li><Link to="/">首页</Link></li>
           <li><Link to="/order">订单列表</Link></li>
           <li><Link to="/info">个人中心</Link></li>
